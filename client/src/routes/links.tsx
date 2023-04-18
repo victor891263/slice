bcf3e1ac7d305f3181e5a8bb37c0ccd700b12cc6
victor <thalinhtet000@gmail.com>
2023-04-18 19:50:18 +0630

    Improved design

--- client/src/routes/links.tsx
+++ client/src/routes/links.tsx
@@ -77,9 +77,9 @@ export default function Links() {
             <div className="h-screen flex flex-col items-center px-6 text-gray-700 dark:text-gray-300">
                 <div className="max-w-screen-lg w-full py-16 my-auto">
                     <h1 className="sm:text-3xl text-2xl text-center font-extrabold tracking-tight text-black dark:text-white">All shortened URLs</h1>
-                    <Link to="/" className="block mt-2 w-fit mx-auto text-indigo-600 dark:text-indigo-400">Get started</Link>
-                    <div className="mt-9 mb-12 relative max-w-sm mx-auto">
-                        <input onChange={e => setSearchKeyword(e.target.value)} value={searchKeyword} type="text" name="search" placeholder="What's your URL?" className="w-full rounded bg-transparent border border-gray-300 py-2.5 px-3.5 outline-0 focus:border-indigo-600 dark:border-gray-700 dark:focus:border-indigo-400 dark:placeholder:text-gray-600" />
+                    <Link to="/" className="sm:mt-1.5 mt-1 block w-fit mx-auto text-indigo-600 dark:text-indigo-400">Get started</Link>
+                    <div className="sm:mb-9 mb-3 mt-8 relative max-w-sm mx-auto">
+                        <input onChange={e => setSearchKeyword(e.target.value)} value={searchKeyword} type="text" name="search" placeholder="What's your URL?" className="w-full shadow-sm rounded bg-transparent border border-gray-300 py-2 px-3 outline-0 focus:border-indigo-600 dark:border-gray-700 dark:focus:border-indigo-400 dark:placeholder:text-gray-600" />
                         <div className="absolute top-0 right-0 pr-2.5 h-full flex items-center">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 text-gray-400 dark:text-gray-600">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
@@ -99,22 +99,22 @@ export default function Links() {
                             </div>
                         ):(
                             sortedUrls.length > 0 ? (
-                                <div className="overflow-x-auto rounded-md border border-gray-300 dark:border-gray-700">
+                                <div className="sm:rounded-md rounded shadow-sm overflow-x-auto border border-gray-300 dark:border-gray-700">
                                     <table className="min-w-full divide-y divide-gray-300 dark:divide-gray-700">
                                         <thead className="bg-gray-100 dark:bg-gray-800">
                                         <tr className="text-gray-500 text-sm">
-                                            <th className="whitespace-nowrap px-4 py-3 font-normal text-left">Full URL</th>
-                                            <th className="whitespace-nowrap px-4 py-3 font-normal text-left">Short URL</th>
-                                            <th className="whitespace-nowrap px-4 py-3 font-normal text-left">
+                                            <th className="whitespace-nowrap px-3 py-2.5 font-normal text-left">Full URL</th>
+                                            <th className="whitespace-nowrap px-3 py-2.5 font-normal text-left">Short URL</th>
+                                            <th className="whitespace-nowrap px-3 py-2.5 font-normal text-left">
                                                 <div className="flex items-center">
-                                                    <span className="mr-1">Clicks</span>
+                                                    <span className="mr-0.5">Clicks</span>
                                                     <button onClick={() => setSortBy({ title: 'clicks', asc: true })}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={"w-4 h-4" + ((sortBy.title === 'clicks' && sortBy.asc) ? " text-indigo-600 dark:text-indigo-400" : "")}><path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m0 0l6.75-6.75M12 19.5l-6.75-6.75" /></svg></button>
                                                     <button onClick={() => setSortBy({ title: 'clicks', asc: false })}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={"w-4 h-4" + ((sortBy.title === 'clicks' && !sortBy.asc) ? " text-indigo-600 dark:text-indigo-400" : "")}><path strokeLinecap="round" strokeLinejoin="round" d="M12 19.5v-15m0 0l-6.75 6.75M12 4.5l6.75 6.75" /></svg></button>
                                                 </div>
                                             </th>
-                                            <th className="whitespace-nowrap px-4 py-3 font-normal text-left">
+                                            <th className="whitespace-nowrap px-3 py-2.5 font-normal text-left">
                                                 <div className="flex items-center">
-                                                    <span className="mr-1">Created</span>
+                                                    <span className="mr-0.5">Created</span>
                                                     <button onClick={() => setSortBy({ title: 'createdOn', asc: true })}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={"w-4 h-4" + ((sortBy.title === 'createdOn' && sortBy.asc) ? " text-indigo-600 dark:text-indigo-400" : "")}><path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m0 0l6.75-6.75M12 19.5l-6.75-6.75" /></svg></button>
                                                     <button onClick={() => setSortBy({ title: 'createdOn', asc: false })}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={"w-4 h-4" + ((sortBy.title === 'createdOn' && !sortBy.asc) ? " text-indigo-600 dark:text-indigo-400" : "")}><path strokeLinecap="round" strokeLinejoin="round" d="M12 19.5v-15m0 0l-6.75 6.75M12 4.5l6.75 6.75" /></svg></button>
                                                 </div>
@@ -124,14 +124,14 @@ export default function Links() {
                                         <tbody className="divide-y divide-gray-300 dark:divide-gray-700">
                                         {sortedUrls.map((url, index) => (
                                             <tr className="" key={index}>
-                                                <td className="whitespace-nowrap px-4 py-3.5 text-indigo-600 dark:text-indigo-400">
+                                                <td className="whitespace-nowrap px-3 py-2.5 text-indigo-600 dark:text-indigo-400">
                                                     <a href={url.full} target="_blank" rel="noreferrer">{url.full.length > 40 ? (url.full.slice(0, 40) + '...') : url.full}</a>
                                                 </td>
-                                                <td className="whitespace-nowrap px-4 py-3.5 text-indigo-600 dark:text-indigo-400">
+                                                <td className="whitespace-nowrap px-3 py-2.5 text-indigo-600 dark:text-indigo-400">
                                                     <a href={process.env.REACT_APP_API_ENDPOINT + url.short} target="_blank" rel="noreferrer">{process.env.REACT_APP_API_ENDPOINT + url.short}</a>
                                                 </td>
-                                                <td className="whitespace-nowrap px-4 py-3.5 font-bold">{url.clicks}</td>
-                                                <td className="whitespace-nowrap px-4 py-3.5">{setTimeLabel(url.createdOn)} ago</td>
+                                                <td className="whitespace-nowrap px-3 py-2.5 font-bold">{url.clicks}</td>
+                                                <td className="whitespace-nowrap px-3 py-2.5">{setTimeLabel(url.createdOn)} ago</td>
                                             </tr>
                                         ))}
                                         </tbody>
@@ -148,7 +148,7 @@ export default function Links() {
                         )
                     )}
 
-                    <div className="mt-8 flex items-center justify-center gap-3">
+                    <div className="mt-6 flex items-center justify-center gap-2">
                         <ThemeButton />
                         <a href="https://github.com/victor891263/slice" target="_blank" rel="noreferrer"><svg className="w-6 h-6 fill-current" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12.026 2C7.13295 1.99937 2.96183 5.54799 2.17842 10.3779C1.395 15.2079 4.23061 19.893 8.87302 21.439C9.37302 21.529 9.55202 21.222 9.55202 20.958C9.55202 20.721 9.54402 20.093 9.54102 19.258C6.76602 19.858 6.18002 17.92 6.18002 17.92C5.99733 17.317 5.60459 16.7993 5.07302 16.461C4.17302 15.842 5.14202 15.856 5.14202 15.856C5.78269 15.9438 6.34657 16.3235 6.66902 16.884C6.94195 17.3803 7.40177 17.747 7.94632 17.9026C8.49087 18.0583 9.07503 17.99 9.56902 17.713C9.61544 17.207 9.84055 16.7341 10.204 16.379C7.99002 16.128 5.66202 15.272 5.66202 11.449C5.64973 10.4602 6.01691 9.5043 6.68802 8.778C6.38437 7.91731 6.42013 6.97325 6.78802 6.138C6.78802 6.138 7.62502 5.869 9.53002 7.159C11.1639 6.71101 12.8882 6.71101 14.522 7.159C16.428 5.868 17.264 6.138 17.264 6.138C17.6336 6.97286 17.6694 7.91757 17.364 8.778C18.0376 9.50423 18.4045 10.4626 18.388 11.453C18.388 15.286 16.058 16.128 13.836 16.375C14.3153 16.8651 14.5612 17.5373 14.511 18.221C14.511 19.555 14.499 20.631 14.499 20.958C14.499 21.225 14.677 21.535 15.186 21.437C19.8265 19.8884 22.6591 15.203 21.874 10.3743C21.089 5.54565 16.9181 1.99888 12.026 2Z"></path></svg></a>
                     </div>
--- client/src/routes/root.tsx
+++ client/src/routes/root.tsx
@@ -55,9 +55,9 @@ export default function Root() {
             <div className="h-screen flex flex-col items-center px-6 text-gray-700 dark:text-gray-300">
                 <div className="max-w-2xl w-full h-screen py-20 flex flex-col justify-center">
                     <h1 className="sm:text-4xl text-3xl text-center font-extrabold tracking-tight text-black dark:text-white">Shorten your URL</h1>
-                    <p className="mt-4 text-center max-w-lg text-lg mx-auto">Use Slice to transform long, ugly links into nice, memorable and trackable short URLs, which can be posted anywhere.</p>
-                    <form onSubmit={submitLink} className="mt-7 grid min-[420px]:grid-cols-[auto_max-content] gap-x-2 gap-y-2.5 w-full">
-                        <input disabled={isLoading} className="disabled:bg-gray-100 disabled:text-gray-400 dark:disabled:bg-gray-800 dark:disabled-text-gray-600 rounded border border-gray-300 bg-transparent py-3 px-4 outline-0 placeholder:text-gray-400 focus:border-indigo-600 dark:border-gray-700 dark:focus:border-indigo-400 dark:placeholder:text-gray-600" required={true} type="url" name="fullUrl" id="fullUrl" placeholder="Enter your URL"/>
+                    <p className="sm:mt-4 mt-3 text-center max-w-lg text-lg mx-auto">Use Slice to transform long, ugly links into nice, memorable and trackable short URLs, which can be posted anywhere.</p>
+                    <form onSubmit={submitLink} className="mt-6 grid min-[420px]:grid-cols-[auto_max-content] gap-2 w-full">
+                        <input disabled={isLoading} className="disabled:bg-gray-100 disabled:text-gray-400 dark:disabled:bg-gray-800 dark:disabled-text-gray-600 shadow-sm rounded border border-gray-300 bg-transparent py-3 px-4 outline-0 placeholder:text-gray-400 focus:border-indigo-600 dark:border-gray-700 dark:focus:border-indigo-400 dark:placeholder:text-gray-600" required={true} type="url" name="fullUrl" id="fullUrl" placeholder="Enter your URL"/>
                         <button disabled={isLoading} className="sm:w-28 flex items-center justify-center gap-2 rounded bg-indigo-600 text-white font-semibold py-3 focus:outline outline-2 outline-indigo-600 outline-offset-2 dark:bg-indigo-400 dark:text-black dark:outline-indigo-400" type="submit">
                             {isLoading ? (
                                 <span>Working...</span>
@@ -72,14 +72,13 @@ export default function Root() {
                         </button>
                     </form>
                     {shortenedUrl && (
-                        <div className="mt-4 bg-gray-100 rounded py-4 px-5 flex items-center justify-between dark:bg-gray-800">
-                            <div>Your link: <span className="font-bold">{shortenedUrl}</span></div>
-                            <button onClick={copyLink} className="font-semibold text-indigo-600 dark:text-indigo-400">Copy
-                            </button>
+                        <div className="sm:mt-3 mt-2 bg-gray-100 rounded py-4 px-5 flex items-center justify-between dark:bg-gray-800">
+                            <div className="break-all">Your link: <span className="font-bold">{shortenedUrl}</span></div>
+                            <button onClick={copyLink} className="max-sm:hidden text-indigo-600 dark:text-indigo-400">Copy</button>
                         </div>
                     )}
-                    <Link to='/links' className="text-indigo-600 mt-7 w-fit mx-auto dark:text-indigo-400">View all shortened links</Link>
-                    <div className="mt-6 flex items-center justify-center gap-3">
+                    <Link to='/links' className="text-indigo-600 mt-5 w-fit mx-auto dark:text-indigo-400">View all shortened links</Link>
+                    <div className="mt-5 flex items-center justify-center gap-2">
                         <ThemeButton />
                         <a href="https://github.com/victor891263/slice" target="_blank" rel="noreferrer"><svg className="w-6 h-6 fill-current" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12.026 2C7.13295 1.99937 2.96183 5.54799 2.17842 10.3779C1.395 15.2079 4.23061 19.893 8.87302 21.439C9.37302 21.529 9.55202 21.222 9.55202 20.958C9.55202 20.721 9.54402 20.093 9.54102 19.258C6.76602 19.858 6.18002 17.92 6.18002 17.92C5.99733 17.317 5.60459 16.7993 5.07302 16.461C4.17302 15.842 5.14202 15.856 5.14202 15.856C5.78269 15.9438 6.34657 16.3235 6.66902 16.884C6.94195 17.3803 7.40177 17.747 7.94632 17.9026C8.49087 18.0583 9.07503 17.99 9.56902 17.713C9.61544 17.207 9.84055 16.7341 10.204 16.379C7.99002 16.128 5.66202 15.272 5.66202 11.449C5.64973 10.4602 6.01691 9.5043 6.68802 8.778C6.38437 7.91731 6.42013 6.97325 6.78802 6.138C6.78802 6.138 7.62502 5.869 9.53002 7.159C11.1639 6.71101 12.8882 6.71101 14.522 7.159C16.428 5.868 17.264 6.138 17.264 6.138C17.6336 6.97286 17.6694 7.91757 17.364 8.778C18.0376 9.50423 18.4045 10.4626 18.388 11.453C18.388 15.286 16.058 16.128 13.836 16.375C14.3153 16.8651 14.5612 17.5373 14.511 18.221C14.511 19.555 14.499 20.631 14.499 20.958C14.499 21.225 14.677 21.535 15.186 21.437C19.8265 19.8884 22.6591 15.203 21.874 10.3743C21.089 5.54565 16.9181 1.99888 12.026 2Z"></path></svg></a>
                     </div>
